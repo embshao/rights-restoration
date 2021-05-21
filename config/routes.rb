d0b628aa9a7dce6a5ab0a241f95186cb6bac81c2
em barnard-shao <em.barnard.shao@gmail.com>
2021-05-21 00:13:46 -0400

    Add footer

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :states, only: [:index, :create]
   get "/off_board", to: "states#off_board", as: :off_board #off_board/:id
   get "/states/:id", to: "states#on_board", as: :on_board #states/:id or just /az
+
+  # screener questions
+  # get "/az/"
 end
